ebe7092f7cbce34e43f6a44a9daeb52c97764b3b
ertanusta <ertanusta@outlook.com>
2021-12-16 23:43:35 +0300

    cargoservice test düzenlemesi

--- tests/Services/CargoServiceTest.php
+++ tests/Services/CargoServiceTest.php
@@ -6,34 +6,66 @@ use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Contracts\HttpClient\ResponseInterface;
 use Trendyol\ApiBundle\Client\TrendyolClient;
+use Trendyol\ApiBundle\Factories\TrendyolUrlFactory;
 use Trendyol\ApiBundle\Services\CargoService;
 
 class CargoServiceTest extends TestCase
 {
-	private function getClientMock()
-	{
-		return $this->createMock(TrendyolClient::class);
-	}
+    public const URL = "url";
+    private function getClientMock()
+    {
+        return $this->getMockBuilder(TrendyolClient::class)
+            ->enableOriginalConstructor()
+            ->setConstructorArgs(["sellerId", "appKey", "appSecret", "integrator"])
+            ->getMock();
+    }
 
-	public function testGetShipmentProviders()
-	{
-		$client = $this->getClientMock();
-		$client
-			->expects($this->once())
-			->method('request')
-			->with(CargoService::SHIPMENT_PROVIDERS_ENDPOINT, Request::METHOD_GET, [], []);
-		$cargoService = new CargoService($client);
-		$this->assertInstanceOf(ResponseInterface::class, $cargoService->getShipmentProviders());
-	}
+    private function getUrlFactoryMock()
+    {
+        return $this->getMockBuilder(TrendyolUrlFactory::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+    }
+
+    public function testGetShipmentProviders()
+    {
+        $client = $this->getClientMock();
+        $urlFactory = $this->getUrlFactoryMock();
+
+        $urlFactory
+            ->expects($this->once())
+            ->method('createUrl')
+            ->with(CargoService::SHIPMENT_PROVIDERS)
+            ->willReturn(self::URL);
+
+        $client
+            ->expects($this->once())
+            ->method('request')
+            ->with(self::URL, Request::METHOD_GET, [], []);
+
+
+        $cargoService = new CargoService($client, $urlFactory);
+        $this->assertInstanceOf(ResponseInterface::class, $cargoService->getShipmentProviders());
+    }
 
 	public function testGetSuppliersAddresses()
 	{
-		$client = $this->getClientMock();
-		$client
-			->expects($this->once())
-			->method('request')
-			->with(CargoService::SUPPLIERS_ADDRESSES_ENDPOINT, Request::METHOD_GET, [], []);
-		$cargoService = new CargoService($client);
+        $client = $this->getClientMock();
+        $urlFactory = $this->getUrlFactoryMock();
+
+        $urlFactory
+            ->expects($this->once())
+            ->method('createUrl')
+            ->with(CargoService::SUPPLIERS_ADDRESSES)
+            ->willReturn(self::URL);
+
+        $client
+            ->expects($this->once())
+            ->method('request')
+            ->with(self::URL, Request::METHOD_GET, [], []);
+
+        $cargoService = new CargoService($client, $urlFactory);
 		$this->assertInstanceOf(ResponseInterface::class, $cargoService->getSuppliersAddresses());
 	}
 }
